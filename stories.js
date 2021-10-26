4e07c88a04bcb3e06d59dd259c17f72cd599bf0a
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2021-10-26 17:59:58 +0200

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,17 @@
+stories = [
+[
+"Mert", `
+Det var en gång en liten snigelkott, det är en taggig snigel, han bodde i en liten rövhåla mitt i en skog i Finland.
+En dag när han skulle gå till bageriet för att köpa bröd stötte han på sin bästa vän, Borp. Båda blev så glada att de genast sa hej, 
+och sniglade sedan runt lite på varandra. Men medans de gjorde det så SNODDE Mert en brödbit från Borp. Det gjorde så att Mert inte längre behövde köpa bröd,
+istället kunde han köpa godis som han genast sög upp i röven. Det var gott, tänkte han nöjt.
+Men då såg han henne, Mertalina, en rosa snigelkott. När han tittade på hennes boobas tänkte han bara omnomnomnomnom.
+Hejsan Mertalina, sa han med en kåt röst.
+HEJSAN UWU, sa hon. Sen knullade de i flera veckor men helt plötsligt så fastnade hans speciella tagg (kuken) i henne, det tyckte Mert om. Slut!
+
+`],[
+"Mert", `
+test
+
+`]
+]
