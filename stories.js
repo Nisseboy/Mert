fca845027855f3addd46a9083c42266dd58b3c09
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2021-10-26 18:39:43 +0200

    Update stories.js

--- stories.js
+++ stories.js
@@ -2,7 +2,7 @@ stories = [
 [
 "Mert", `
 Det var en gång en liten snigelkott, det är en taggig snigel, han bodde i en liten rövhåla mitt i en skog i Finland.
-En dag när han skulle gå till bageriet för att köpa bröd stötte han på sin bästa vän, Borp. Båda blev så glada att de genast sa hej, 
+En dag när han skulle gå till bageriet för att köpa bröd stötte han på sin bästa vän, Borp. Han var grön, till skillnad från Mert, som var blå. Båda blev så glada att de genast sa hej, 
 och sniglade sedan runt lite på varandra. Men medans de gjorde det så SNODDE Mert en brödbit från Borp. Det gjorde så att Mert inte längre behövde köpa bröd,
 istället kunde han köpa godis som han genast sög upp i röven. Det var gott, tänkte han nöjt.
 Men då såg han henne, Mertalina, en rosa snigelkott. När han tittade på hennes boobas tänkte han bara omnomnomnomnom.
