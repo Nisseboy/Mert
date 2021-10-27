e547fc5ad5f342e368e9a63eb3e73a41c2944176
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2021-10-27 20:16:07 +0200

    Update stories.js

--- stories.js
+++ stories.js
@@ -10,6 +10,21 @@ Hejsan Mertalina, sa han med en kåt röst.
 HEJSAN UWU, sa hon. Sen knullade de i flera veckor men helt plötsligt så fastnade hans speciella tagg (kuken) i henne, det tyckte Mert om. Slut!
 
 `],[
+"Mert 2 - Elektrisk Bögalög", `
+Det var en helt vanlig dag när Mert upptäckte att han hade ont i kuken, hmm, tänkte han, hur ska jag lösa det här?
+Just då snubblade han ut ur sin rövhåla och slog i trädet han planterade dagen innan, och den sögs rakt in i hans kukhål (bruh).
+Det sved ganska gött, tyckte han nöjt. Men tyvärr var det så att trädet var fullvuxet så. Det betydde att han fick ett helt träd i kuken, 
+eftersom Mert är lika liten som en snigel så kunde trädet inte riktigt få plats i honom så den behövde åka ut någonstans. Det blev röven. Nu hade han alltså ett helt träd
+in i kuken och ut genom röven (bruh igen). När han sedan skulle gå till bageriet igen slog han ned varenda lyktstolpe i hela staden, det blev Mert som fick betala för skadorna
+tyvärr 😭. Det gjorde Mert väldigt ledsen, så han ville inte längre köpa bröd, inte för att han hade råd med det ändå (ett tredje bruh).
+MEN DÅ HÄNDE NÅGOT FANTASTISK!! Han snubblade vilket ledde till att trädet skjöts ut ur hans röv VÄLDIGT snabbt. Den träffade sorgligt nog en skola, 
+34 barn dog, varav 4 av deras huvuden fastnade i trädet och 41 barn blev allvarligt skadade. Deras familjer blev väldigt sura 😡.
+De ringde genast Barrack Obama och bad honom stämma Mert. Obama flög genast till Finland för att kasta ett stämband på honom men till sin förfasan sög han upp det i sitt rövhål 😎.
+Då plockade Obama fram tre advokater som också skulle kasta stämband på honom. Men då, precis i rättans tid kom trädet som tidigare skjutits ut ur hans röv.
+Genast fastnade trädet mellan Obamas tänder och han dog en snabb död. Bruh, tänkte hans familj just då, då märkte Mert däremot att han inte längre hade ont i kuken 🤗 yippie!!!
+Slut!
+
+`],[
 "Mert", `
 test
 
