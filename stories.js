de0743ef17f60148fe41de9888dd8cde9d2a871e
Nisseboy <nils@tjarnlunds.se>
2022-08-28 16:16:17 +0200

    first

---
+++ stories.js
@@ -0,0 +1,70 @@
+stories = [
+[
+"Mert", `
+Det var en gång en liten snigelkott, det är en taggig snigel, han bodde i en liten rövhåla mitt i en skog i Finland.
+En dag när han skulle gå till bageriet för att köpa bröd stötte han på sin bästa vän, Borp. Han var grön, till skillnad från Mert, som var blå. Båda blev så glada att de genast sa hej, 
+och sniglade sedan runt lite på varandra. Men medans de gjorde det så SNODDE Mert en brödbit från Borp. Det gjorde så att Mert inte längre behövde köpa bröd,
+istället kunde han köpa godis som han genast sög upp i röven. Det var gott, tänkte han nöjt.
+Men då såg han henne, Mertalina, en rosa snigelkott. När han tittade på hennes boobas tänkte han bara omnomnomnomnom.
+Hejsan Mertalina, sa han med en kåt röst.
+HEJSAN UWU, sa hon. Sen knullade de i flera veckor men helt plötsligt så fastnade hans speciella tagg (kuken) i henne, det tyckte Mert om. Slut!
+
+`],[
+"Mert 2 - Elektrisk Bögalög", `
+Det var en helt vanlig dag när Mert upptäckte att han hade ont i kuken, hmm, tänkte han, hur ska jag lösa det här?
+Just då snubblade han ut ur sin rövhåla och slog i trädet han planterade dagen innan, och den sögs rakt in i hans kukhål (bruh).
+Det sved ganska gött, tyckte han nöjt. Men tyvärr var det så att trädet var fullvuxet så. Det betydde att han fick ett helt träd i kuken, 
+eftersom Mert är lika liten som en snigel så kunde trädet inte riktigt få plats i honom så den behövde åka ut någonstans. Det blev röven. Nu hade han alltså ett helt träd
+in i kuken och ut genom röven (bruh igen). När han sedan skulle gå till bageriet igen slog han ned varenda lyktstolpe i hela staden, det blev Mert som fick betala för skadorna
+tyvärr 😭. Det gjorde Mert väldigt ledsen, så han ville inte längre köpa bröd, inte för att han hade råd med det ändå (ett tredje bruh).
+MEN DÅ HÄNDE NÅGOT FANTASTISK!! Han snubblade vilket ledde till att trädet skjöts ut ur hans röv VÄLDIGT snabbt. Den träffade sorgligt nog en skola, 
+34 barn dog, varav 4 av deras huvuden fastnade i trädet och 41 barn blev allvarligt skadade. Deras familjer blev väldigt sura 😡.
+De ringde genast Barrack Obama och bad honom stämma Mert. Obama flög genast till Finland för att kasta ett stämband på honom men till sin förfasan sög han upp det i sitt rövhål 😎.
+Då plockade Obama fram tre advokater som också skulle kasta stämband på honom. Men då, precis i rättans tid kom trädet som tidigare skjutits ut ur hans röv.
+Genast fastnade trädet mellan Obamas tänder och han dog en snabb död. Bruh, tänkte hans familj just då, då märkte Mert däremot att han inte längre hade ont i kuken 🤗 yippie!!!
+Slut!
+
+`],[
+"Mert 3 - Bögskalet", `
+Den här gången åkte Mert i ett racingskal 🏎, Borp var också med 👌. De hade nämligen planerat en roadtrip till Skåne, det skulle blir jättekul, tänkte de nöjt.
+Det de däremot inte visste var att det just då flög en kåt runkapa flygandes mot dem i väldans fart. BONK lät det när apans magnum dong penetrerade skaldörren.
+Det tog tyvärr inte slut där däremot, när apan träffade skaldörren blev han så hård att hans massiva tegelsten av kuk våldsamt sögs upp i Borps röv, ut ur hans näsa och sedan
+störtade in i Merts oförberedda rövhål. Aj, tänkte Borp lite ledset 🙁. Hejsan, jag heter Knorrp, hälsade apan glatt. Han gungade fram och tillbaka lite och stönade som om han precis 
+hade missat sitt flyg 😩. Sedan slet han ut sin kuk så snabbt att hela skalet skakade, alla dog nästan av kraften av hans fruktfulla bärstjälk. Sshhchschlorp, lät det.
+Då hände däremot någonting VÄLDIGT konstigt, Boris Johnson kom in i bilen. Genast absorberade han genom röven pizzan Mert hade tänkt äta till lunch 🍕. Mert slog våldsamt
+och fylld av ilska till Boris i hans bastanta bollar 😡. Boris tjöt ut av smärta och hans bollar exploderade, vilket ledde till att bilen nu var fylld av sperma ⬜⬜.
+Knorrp blev också träffad konstigt nog. Nånting väldigt kusligt hände också 👻, Det sögs upp i Boris näsa. Han kunde inte andas och 
+eftersom Mert inte kunde se något (på grund av sperman), så körde han rakt in i ett kukformat träd. Då dog Boris av död. Eftersom Mert, Borp och Knorrp var hungriga
+så grillade de Boris. Mmm så gott, tänkte Mert nöjt och petade snabbt in grillpinen i sin röv. Slut!
+`],[
+"Mert 4 - Gubbjäveln", `
+Idag hade Mert fått nog men äventyr, så han tänkte att han bara skulle sitta mitt ute i vägen och läsa om Bob, sin kusin. Bob var mot alla odds också en snigelkott, wow😮. När han satt
+där ute mitt på vägen och chillade kom en sur gubbjävel. Hej, jag är sur 😡, sa han surt. Därefter sniglade han sig en bit längs med vägen, direkt blev han överkörd. 
+Det gjorde att han dog till döds ☠. Han sura tantjävel till fru kom därför snabbt och slog Mert. Aj, sa han fyllt av smärta och tryckte in sin stenhårda kuk i hennes öga?!
+aj, tänkte hon också. Efterswom snigelkottar är så hala kunde Mert våldsamt suga in tantjäveln i sin tajta röv, oof 🙄. Inuti Merts röv var det tydligen varmt och mjukt. 
+Så mjukt att hon somnade på direketen. Väl utanför Mert röv gick Mert tillbaka till sin rövhåla (den i Finland alltså). På vägen hem köpte han en bit bröd som var ganska varm.
+Väl hemma åt han upp brödet genom att suga upp det i sin röv, tantjäveln dog. Den var så varm att han fick brännskador i röven. För att se hur stora brännskadorna var tryckte han
+upp näven i sin röv ✊. Brännskadan var ganska stor, tänkte hna, så han hällde en massa coca cola i röven.
+Det började däremot bubbla så våldsamt att det sprutade flera liter cola överallt, då blev hans hus fyllt med cola. Slut!
+`],[
+"Mert 5 - Rövmobilen", `
+Mert hade slut på mjölk i kylen, det var inte alls bra 😔. Därför var han tvugen att dra till coop, jag vet inte om det finns i Finland men ändå.
+När han gick omkring i coop ringde hans röv däremot vilket var lite jobbigt, han svarade och sa hejdå. Då märkte han att det var tantjävelns telefon. 
+Han stoppade därför tillbaka telefonen i röven och fortsatte handla, det fanns ett stort hål framför mjölken med en massa borttappade pappor i sig. 😡 Han sträckte sig tvärs över
+alla pappor för att hämta mjölken MEN DÅ snubblade han och ramlade nästan ner i helåt, det hade varit ett stort jävla bruh moment för Mert, bruh 🙄. 
+Han skyndade sig snabbt till kassan för att checka ut och kassörskan skannade alla hans fina mjölkpaket som han hade köpt. Hej, sa kassörskan och dog av kåthet på grund av Mert 😩.
+Hej hej, sa Mert tillbaka och kassörskan tänkte säga hej igen men det gick tyvärr inte på grund av att hon tyvärr var död 😔. Nu kunde Mert äntligen återvända till sin rövhåla och äta sina ouppätna flingor.
+Istället för att äta dem så tryckte han tyvärr upp dem i röven. Då blev flingorna tyvärr väldigt blöta 😔 och äckliga. Det gör inget, sa Mert lugnt och med flingor i röven. Slut!
+`]
+]
+
+
+/*
+
+,[
+"Mert", `
+test
+
+`]
+
+*/
