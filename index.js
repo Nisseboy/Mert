2721e7872324064d74f1a96c1a23f5ba32b98702
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2022-10-11 12:56:21 +0200

    sexsexsexsex

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let title = document.getElementsByClassName('title')[0];
 let teskt = document.getElementsByClassName('teskt')[0];
+let buttons = document.getElementsByClassName('buttons');
 
 let story = 0;
 
@@ -17,4 +18,7 @@ function changeStory(offset) {
     story = Math.max(Math.min(story, stories.length - 1), 0);
 
     setStory(story);
+
+    buttons[0].style.display = ((story > 0)?"block":"none");
+    buttons[1].style.display = ((story < stories.length - 1)?"block":"none");
 }
